9f584c778916498a80a569801c27ad4717e9e3ea
Jakub Nyckowski <jakub.nyckowski@goteleport.com>
2021-12-02 22:51:12 -0500

    Fix one more failing test.

--- lib/config/configuration_test.go
+++ lib/config/configuration_test.go
@@ -1858,7 +1858,7 @@ func TestDatabaseCLIFlags(t *testing.T) {
 					},
 				},
 				TLS: service.DatabaseTLS{
-					CACert: fixtures.LocalhostCert,
+					Mode: service.VerifyFull,
 				},
 			},
 		},
@@ -1974,9 +1974,10 @@ func TestDatabaseCLIFlags(t *testing.T) {
 				require.Contains(t, err.Error(), tt.outError)
 			} else {
 				require.NoError(t, err)
-				require.Equal(t, []service.Database{
-					tt.outDatabase,
-				}, config.Databases.Databases)
+				require.Equal(t,
+					config.Databases.Databases,
+					[]service.Database{tt.outDatabase},
+				)
 			}
 		})
 	}
